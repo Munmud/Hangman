684856f9ee190ccd35169979aa57b4c7ff0e18e2
Munmud <moontasir042@gmail.com>
2020-08-30 09:13:42 +0600

    all js event listener added

--- js/main.js
+++ js/main.js
@@ -4,13 +4,12 @@ const playAgainBtn = document.getElementById('play-again') ;
 const popup = document.getElementById('popup-container') ;
 const notification = document.getElementById('notification-container') ;
 const finalMessage = document.getElementById('final-message') ;
+const figureParts = document.querySelectorAll('.figure-part') ;
 
-const figureParts = document.querySelectorAll('figure-part') ;
 
 const words = ['application' , 'programming' , 'interface' , 'wizard'] ;
 
 let selectedWord = words[Math.floor(Math.random()*words.length)] ;
-console.log(selectedWord); 
 
 const correctLetters = [] ;
 const wrongLetters = [] ;
@@ -36,5 +35,77 @@ function displayWord() {
 
 
 }
+
+// Update woring letters
+function updateWrongLettersEL () {
+    // Display wrong letters
+    wrongLettersEl.innerHTML = `
+        ${wrongLetters.length >0 ? '<p>Wrong</p>' : ''}
+        ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    ` ;
+
+    // Display parts
+    figureParts.forEach( (part,index) =>{
+        const errors = wrongLetters.length ;
+        if (index < errors){
+            part.style.display = 'block' ;
+        }
+        else {
+            part.style.display = 'none' ;
+        }
+    }) ;
+
+    // Check if lost
+    if (wrongLetters.length === figureParts.length){
+        finalMessage.innerText = 'Unfortunately you lost' ;
+        popup.style.display = 'flex' ;
+    }
+}
+
+// Show notification 
+function showNotification() {
+    notification.classList.add('show') ;
+    setTimeout(()=>{
+        notification.classList.remove('show') ;
+    },2000) ;
+}
+
+// Keydown letter press
+window.addEventListener('keydown' , e => {
+    if (e.keyCode>=65 && e.keyCode <=90){
+        const letter = e.key ;
+
+        if (selectedWord.includes(letter)){
+            if(!correctLetters.includes(letter)){
+                correctLetters.push(letter) ;
+                displayWord() ;
+            }
+            else {
+                showNotification() ;
+            }
+
+        }
+        else {
+            if(!wrongLetters.includes(letter)){
+                wrongLetters.push(letter) ;
+                updateWrongLettersEL() ;
+            }
+            else {
+                showNotification() ;
+            }
+        }
+    }
+}) ;
+
+// Restart game and play agian
+playAgainBtn.addEventListener('click' , () => {
+    // Empty array ;
+    correctLetters.splice(0) ;
+    wrongLetters.splice(0) ;
+    selectedWord = words[Math.floor(Math.random()*words.length)] ;
+    displayWord() ;
+    updateWrongLettersEL() ;
+    popup.style.display = 'none' ;
+}) ;
 
 displayWord();
